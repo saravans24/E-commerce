763c8085158b78a2facf2974032b21e96a95612e
saravanan <saravanans2496@gmail.com>
2023-07-11 08:34:10 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className=''> 
+      <Header/>
+      <main className='pt-16'>
+        <Outlet/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
